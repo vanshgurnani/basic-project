2f73aa07abe9d79ae2fb007d8036c3b925a4e60b
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-12-31 20:00:33 +0530

    push

---
+++ backend/index.js
@@ -0,0 +1,105 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// const Note = require ('./models/notesmodel');
+const User = require('./models/usersmodel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+// const Summary = require('./models/summarymodel');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://text-util-iota.vercel.app',
+    'https://notenews.onrender.com',
+    'https://flask-production-71e4.up.railway.app',
+    'https://text-util-five.vercel.app',
+    'https://text-util-dj8q.vercel.app'
+    // 'https://python-jugp.vercel.app',
+  // Replace with your Vercel frontend URL
+  ];
+  
+  app.use(cors({
+    origin: allowedOrigins,
+  }));
+// app.use(cors());
+app.use(express.json());
+
+// Define database and collection names
+const DB_NAME = 'Notepad';
+
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://gurnanivansh57:iz64rqtBBQss8iQ7@cluster101.nuwewcc.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
+
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+// Registration endpoint
+app.post('/registers', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({ username, email, password: hashedPassword });
+
+    // Save the user to the database
+    await user.save();
+
+    res.status(201).json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: 'Registration failed' });
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    if (user) {
+      // Compare the hashed password with the provided password
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+        // Generate a JWT token with the user's information
+        const token = jwt.sign({userId: user._id, username: user.username }, 'your-secret-key');
+
+        res.status(200).json({ message: 'Login successful', token });
+      } else {
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
+    } else {
+      res.status(401).json({ error: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// Define a route for the home route ("/")
+app.get('/', (req, res) => {
+  res.send('Welcome to the Project API');
+});
+
+// More routes for reading, updating, and deleting notes
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
