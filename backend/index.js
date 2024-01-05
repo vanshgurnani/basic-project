5e36d1ecd86dcc88548933309b7ea5e7ec8185f3
Vansh Gurnani <gurnanivansh57@gmail.com>
2024-01-05 13:43:42 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   
   app.use(cors({
     origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
   }));
 // app.use(cors());
 app.use(express.json());
